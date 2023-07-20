54bf6c52e32a559b687c2b913a444ece149ab1cf
Bruna Finholdt Lamboglia Perez <bruna.lamboglia@nutrien.com>
2023-07-20 10:26:21 -0300

    final alterations

--- Minishop/Controllers/CustomersController.cs
+++ Minishop/Controllers/CustomersController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Minishop.Domain.DTO;
+using Minishop.Domain.DTO.Validation;
 using Minishop.Services;
 
 namespace Minishop.Controllers
@@ -42,10 +43,10 @@ namespace Minishop.Controllers
         }
 
         [HttpGet("{id}")]
-        public async Task<IActionResult> GetById(int id)
+        public async Task<IActionResult> GetById([Id(ErrorMessage = "Valor de Id não condiz com formato esperado")] string id)
         {
             //Validação modelo de entrada
-            var retorno = await _service.PesquisaPorId(id);
+            var retorno = await _service.PesquisaPorId(int.Parse(id));
 
             if (retorno.Sucesso)
                 return Ok(retorno.Conteudo);
